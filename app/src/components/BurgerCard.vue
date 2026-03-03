a36f459360439458572bfeb442df6994c1b2f2c8
Boycool3 <christian.beaz91@gmail.com>
2026-03-03 12:32:26 -0500

    Buns, price counter and some other stuff

---
+++ app/src/components/BurgerCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="burgercard">
+        <h2>{{burger.name}}</h2>
+        <h3>{{burger.price}}$</h3>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    burger:{
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+div{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    height: 300px;
+    width: 200px;
+}
+</style>
--- app/src/views/BurgerMaker.vue
+++ app/src/views/BurgerMaker.vue
@@ -1,19 +1,31 @@
 <template>
+  <div class="SOMANYDIVSAAAA">
+    <div class="burgerwindow">
+      <h1>Current Price: {{burgerprice}}$</h1>
+      <h3>Current Burger:</h3>
+    </div>
   <div class="thing">
-    <p>Burger,,,</p>
-    <h2>Current Price: {{burgerprice}}$</h2>
+    <h1>Toppings</h1>
     <div class="container">
       <ToppingCard v-for="topping in toppings" :key="topping.name" :topping="topping">
         <button @click="Add(topping)">Add To Burger</button>
         <button @click="Nvm(topping)">Remove From Burger</button>
       </ToppingCard>
     </div>
+    <h1>Burger Buns</h1>
+    <div class="container">
+      <BurgerCard v-for="burger in burgers" :key="burger.name" :burger="burger">
+        <button @click="Add(burger)">Use in Burger</button>
+      </BurgerCard>
+    </div>
+  </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import ToppingCard from '@/components/ToppingCard.vue'
+import BurgerCard from '@/components/BurgerCard.vue'
 const toppings = ref([
   { name: 'Tomatoes', price: 420 },
   { name: 'Lettuce', price: 5000 },
@@ -21,32 +33,49 @@ const toppings = ref([
   { name: 'Beef Patty', price: 4 },
   { name: 'Chicken Patty', price: 1200 },
   { name: 'Onions', price: 0.5 },
-  { name: 'Pickle(Singular)', price: 1.79e308 },
+  { name: 'Pickle(Singular)', price: 1e308 },
   { name: 'Eggs', price: 3 },
   { name: 'Bacon', price: 0.2 },
   { name: 'Chicken Cuts', price: 300 },
 ])
-let burgerprice = 0;
+const burgers = ref([
+  { name: 'Basic Bun', price: 10.00 },
+  { name: 'Pretzel Bun', price: 10.00 },
+  { name: 'Egg Bun', price: 10.00 },
+  { name: 'Ink Bun', price: 10.00 },
+])
+let burgerprice = 10;
 function Add(topping){
   burgerprice = burgerprice + topping.price
+  if(burgerprice > 1e308){
+    burgerprice = 1e308
+  }
 }
 function Nvm(topping){
   burgerprice = burgerprice - topping.price
-  if(burgerprice < 0){
-    burgerprice = 0
+  if(burgerprice < 10){
+    burgerprice = 10
   }
 }
 </script>
 
 <style scoped>
+h1{
+  text-align: center;
+}
 h2{
   text-align: center;
 }
 .thing {
   border: 15px outset rgb(255, 173, 31);
-  border-style: outset ridge ridge outset;
+  border-style: outset ridge ridge none;
   border-radius: 10px;
   background-color: rgb(255, 207, 124);
+  border-bottom-left-radius: 0px;
+  border-top-left-radius: 0px;
+}
+.SOMANYDIVSAAAA {
+  display: flex;
 }
 .container {
   display: flex;
@@ -68,5 +97,31 @@ h2{
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+}
+.burgercard {
+  border: 33px double rgb(124, 71, 52);
+  background-color: coral;
+  padding: 4px;
+  margin-bottom: 12px;
+  border-radius: 24px;
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.burgerwindow{
+  border: 15px outset rgb(255, 173, 31);
+  border-style: outset none ridge outset;
+  border-radius: 10px;
+  border-bottom-right-radius: 0px;
+  border-top-right-radius: 0px;
+  background-color: rgb(255, 207, 124);
+}
+button{
+  width: 200px;
+  border: 3px rgb(124, 71, 52) solid;
+  border-radius: 5px;
+  background-color: rgb(255, 207, 124);
 }
 </style>
